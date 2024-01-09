26cd8bd54acbd664c0189a8bc49736e2062057a8
Faridun Nuriddinzoda <faridunsmac@Fariduns-MacBook-Pro.local>
2024-01-09 09:40:35 +0600

    xcl

--- excelizer/excel.go
+++ excelizer/excel.go
@@ -30,3 +30,51 @@ func writeToExcelV1() {
 	}
 
 }
+
+func writeToExcelV2() {
+	file := xlsx.NewFile()
+
+	sheet, err := file.AddSheet("Sheet1")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	row := sheet.AddRow()
+	cell := row.AddCell()
+	cell.Value = "I am a cell!"
+
+	cell = row.AddCell()
+	cell.Value = time.Now().String()
+
+	err = file.Save("MyXLSXFile.xlsx")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("File was successfully created!")
+	}
+}
+
+func writeToExcelV3() {
+	file := xlsx.NewFile()
+
+	sheet, err := file.AddSheet("Sheet1")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	row := sheet.AddRow()
+	cell := row.AddCell()
+	cell.Value = "I am a cell!"
+
+	cell = row.AddCell()
+	cell.Value = time.Now().String()
+
+	err = file.Save("MyXLSXFile.xlsx")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("File was successfully created!")
+	}
+}
